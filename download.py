44dc7b0cb1c0a2f667431aa23d1e228a27e6e5b1
G Shreekar <144794118+GShreekar@users.noreply.github.com>
2025-02-02 16:52:26 +0530

    Initial commit

---
+++ download.py
@@ -0,0 +1,19 @@
+# A simple script to download movie posters from IMDB
+import requests
+import os
+from imdb import IMDb
+
+def download(movie_id):
+    id = IMDb()
+    movie = id.get_movie(movie_id)
+    title = movie['title']
+    poster_url = movie['cover url']
+    poster = requests.get(poster_url)
+    poster.raise_for_status()
+    with open(f'{title}.jpg', 'wb') as f:
+        f.write(poster.content)
+    print(f'{title} poster downloaded')
+    
+def main():
+    movie_id = input('Enter IMDB movie ID: ')
+    download(movie_id)
